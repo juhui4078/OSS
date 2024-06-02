242a5510b5b3f1ffcebae818b6663dba56275b19
KimJuHui <juhui4078@hufs.ac.kr>
2024-06-02 13:35:25 +0000

    done

---
+++ Study/month.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#인수가 없으면 오류로 종료
+#인수가 1개면 실행
+  #숫자를 달로 환사하기
+    #1 -> Jan
+    #2 -> Feb
+    #12 -> ...
+#인수가 2개 이상이면 오류로 종료
+#예 : 파일명 month
+
+month="$1"
+
+if [ $# -eq 1 ]; then
+  case $month in
+    1) month="Jan" ;;
+    2) month="Feb" ;;
+    3) month="Mar" ;;
+    4) month="Apr" ;;
+    5) month="May" ;;
+    6) month="Jun" ;;
+    7) month="Jul" ;;
+    8) month="Aug" ;;
+    9) month="Sep" ;;
+    10) month="Oct" ;;
+    11) month="Nov" ;;
+    12) month="Dec" ;;
+  esac
+  
+else
+  echo "입력값 오류"
+  exit 0
+fi  
--- Study/time.sh
+++ Study/time.sh
@@ -14,13 +14,38 @@ if [ $# -eq 1 ]; then
 
   case $hour in
     [1-9])
-    echo "굿모닝";;
+    echo "굿모닝" ;;
     1[0-7])
-    echo "굿애프터눈";;
+    echo "굿애프터눈" ;;
     1[8-9] | 2[0-4])
-    echo "굿이브닝";;
+    echo "굿이브닝" ;;
+    *) ;;
   esac
   
 else
   exit 0
 fi
+
+
+위에 거랑 같은 코드
+
+hour="$1"
+
+if [ $# -eq 1 ]; then
+
+  if [ "$hour" -gt 0 ] && [ "$hour" -le 9 ]; then
+    echo "굿모닝"
+  elif [ "$hour" -gt 10 ] && [ "$hour" -le 17 ]; then
+    echo "굿애프터눈"
+  elif 
+    [ "$hour" -gt 18 ] && [ "$hour" -le 24 ]; then
+    echo "굿이브닝"
+  else
+    echo "입력값 오류"
+  fi
+  exit 0
+  
+else
+  echo "입력값 오류"
+  exit 0
+fi 
