0a777ce0a621c5da98ce7c9a0298961d67a2f00d
KimJuHui <juhui4078@hufs.ac.kr>
2024-06-09 07:59:55 +0000

    done

--- phonebook.sh
+++ phonebook.sh
@@ -12,6 +12,35 @@ fi
 NAME="$1"
 PHONE_NUMBER="$2"
 
+# 성 추출
+LAST_NAME=$(echo "$NAME" | awk '{print $1}')
+
+# 지역 구하기
+AREA_CODE="${FORMATTED_PHONE_NUMBER%%-*}"
+case "$AREA_CODE" in
+    "02")
+        AREA="서울"
+        ;;
+    "031")
+        AREA="경기"
+        ;;
+    "032")
+        AREA="인천"
+        ;;
+    "051")
+        AREA="부산"
+        ;;
+    "055")
+        AREA="거제"
+        ;;
+    "010")
+        AREA="개인 번호"
+        ;;
+    *)
+        AREA="알 수 없는 지역 번호"
+        ;;
+esac
+
 # 전화번호 숫자 판별
 if ! [[ "$PHONE_NUMBER" =~ ^[0-9]+$ ]]; then
     echo "입력값 오류 : 전화번호는 숫자만 입력 가능합니다."
@@ -67,35 +96,6 @@ if grep -q " $FORMATTED_PHONE_NUMBER " "$PHONEBOOK_FILE"; then
         exit 1
     fi
 fi
-
-# 성 추출
-LAST_NAME=$(echo "$NAME" | awk '{print $1}')
-
-# 지역 구하기
-AREA_CODE="${FORMATTED_PHONE_NUMBER%%-*}"
-case "$AREA_CODE" in
-    "02")
-        AREA="서울"
-        ;;
-    "031")
-        AREA="경기"
-        ;;
-    "032")
-        AREA="인천"
-        ;;
-    "051")
-        AREA="부산"
-        ;;
-    "055")
-        AREA="거제"
-        ;;
-    "010")
-        AREA="개인 번호"
-        ;;
-    *)
-        AREA="알 수 없는 지역 번호"
-        ;;
-esac
 
 # 새로운 항목 추가
 echo "$NAME $FORMATTED_PHONE_NUMBER $AREA" >> "$PHONEBOOK_FILE"
